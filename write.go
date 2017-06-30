56935cd0c3c66987f3c05a37717d5fba6799dc71
Ivan Bozhilov <ivanbojilov@gmail.com>
2017-07-01 01:34:52 +0300

    Removing unnececery functions, change the functions to work by reference and add a function for replacing the coefficient in the file depending on its role

--- write.go
+++ write.go
@@ -7,134 +7,79 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
-	"syscall"
 )
 
-//Generate a personalized string in .tex format for the student
-func Write(txt, fn, gr, spec, name, info, date string) string {
-	txt = WriteFN(txt, fn)
-	txt = WriteDate(txt, date)
-	txt = WriteGr(txt, gr)
-	txt = WriteName(txt, name)
-	txt = WriteSpec(txt, spec)
-	txt = WriteInfo(txt, info)
-	txt = WriteRandomMatrix(txt, fn)
-	txt = WriteRandomMatrix(txt, fn)
-	txt = WriteRandomLinearSystem(txt, fn)
-	txt = WriteVector(txt, fn)
-	txt = WriteMatrix4x4(txt, fn)
-	txt = GenerateRandomMatrixOfOperator(txt, fn)
-	return txt
-}
-
-//Replace <*random_matrix3*> with the Generated 4 by 4 matrix
-func WriteMatrix4x4(txt, fn string) string {
-	return strings.Replace(txt, `<*random_matrix3*>`, MaximaMatrixToLaTeX(GenerateMatrix4x4(fn), fn), 1)
+//Fill in the data of the student and details about the test/homework
+//into a *.tex file
+func WriteData(txt, fn, gr, name *string, spec, info, date string) {
+	WriteFN(txt, fn)
+	WriteDate(txt, &date)
+	WriteGr(txt, gr)
+	WriteName(txt, name)
+	WriteSpec(txt, &spec)
+	WriteInfo(txt, &info)
 }
 
 //Write the FN of the student in the document
-func WriteFN(txt, fn string) string {
-	re := regexp.MustCompile(`<\*[Ff][Nn]\*>`)
-	return re.ReplaceAllString(txt, fn)
+func WriteFN(txt, fn *string) {
+	re := regexp.MustCompile(`(?i)<\*fn\*>`)
+	*txt = re.ReplaceAllString(*txt, *fn)
 }
 
 //Write the Group number of the student in the document
-func WriteGr(txt, gr string) string {
-	re := regexp.MustCompile(`<\*[Gg][Rr]\*>`)
-	return re.ReplaceAllString(txt, gr)
+func WriteGr(txt, gr *string) {
+	re := regexp.MustCompile(`(?i)<\*gr\*>`)
+	*txt = re.ReplaceAllString(*txt, *gr)
 }
 
 //Write the course, which the student is taking
-func WriteSpec(txt, spec string) string {
-	re := regexp.MustCompile(`<\*[Ss][Pp][Ee][Cc]\*>`)
-	return re.ReplaceAllString(txt, spec)
+func WriteSpec(txt, spec *string) {
+	re := regexp.MustCompile(`(?i)<\*spec\*>`)
+	*txt = re.ReplaceAllString(*txt, *spec)
 }
 
 //Write the name of the student in the document
-func WriteName(txt, name string) string {
-	re := regexp.MustCompile(`<\*[Nn][Aa][Mm][Ee]\*>`)
-	return re.ReplaceAllString(txt, name)
+func WriteName(txt, name *string) {
+	re := regexp.MustCompile(`(?i)<\*name\*>`)
+	*txt = re.ReplaceAllString(*txt, *name)
 }
 
 //Write info about the homework or the test
-func WriteInfo(txt, info string) string {
-	re := regexp.MustCompile(`<\*[Ii][Nn][Ff][Oo]\*>`)
-	return re.ReplaceAllString(txt, info)
+func WriteInfo(txt, info *string) {
+	re := regexp.MustCompile(`(?i)<\*info\*>`)
+	*txt = re.ReplaceAllString(*txt, *info)
 }
 
 //Write the date, that will be printed on the document
-func WriteDate(txt, date string) string {
-	re := regexp.MustCompile(`<\*[Dd][Aa][Tt][Ee]\*>`)
-	return re.ReplaceAllString(txt, date)
+func WriteDate(txt, date *string) {
+	re := regexp.MustCompile(`(?i)<\*date\*>`)
+	*txt = re.ReplaceAllString(*txt, *date)
 }
 
-//Generates a random matrix from the FN of the student and write it down in the document.
-//Maxima is used for the generation of the matrix.
-func WriteRandomMatrix(txt, fn string) string {
-	re := regexp.MustCompile(`\\pmatrix\{.+\}`)
-	f, err := os.Create("/home/ivan/gocode/src/fmi/automatehw/" + fn + ".mac")
-	defer syscall.Unlink(f.Name()) // Think whether it is neccesary to delete the file
-	if err != nil {
-		panic(err)
+//Replace the coefficient in the file depending on it role. If it is a
+// coefficient infront of a variable then use (c), if it is the leading
+// coefficient the use (lc), if it is a free coefficient then use (fc)
+func WriteCoeffsToFile(paramValue int, paramSign string, txt *string) {
+	if paramValue > 1 {
+		*txt = strings.Replace(*txt, "<*(c)"+paramSign+"*>", "+"+strconv.Itoa(paramValue), -1)
+		*txt = strings.Replace(*txt, "<*(lc)"+paramSign+"*>", strconv.Itoa(paramValue), -1)
+		*txt = strings.Replace(*txt, "<*"+paramSign+"*>", strconv.Itoa(paramValue), -1)
+		*txt = strings.Replace(*txt, "<*(fc)"+paramSign+"*>", "+"+strconv.Itoa(paramValue), -1)
+	} else if paramValue == 1 {
+		*txt = strings.Replace(*txt, "<*(c)"+paramSign+"*>", "+", -1)
+		*txt = strings.Replace(*txt, "<*(lc)"+paramSign+"*>", "", -1)
+		*txt = strings.Replace(*txt, "<*"+paramSign+"*>", "1", -1)
+		*txt = strings.Replace(*txt, "<*(fc)"+paramSign+"*>", "+1", -1)
+	} else if paramValue == -1 {
+		*txt = strings.Replace(*txt, "<*(c)"+paramSign+"*>", "-", -1)
+		*txt = strings.Replace(*txt, "<*(lc)"+paramSign+"*>", "-", -1)
+		*txt = strings.Replace(*txt, "<*"+paramSign+"*>", "-1", -1)
+		*txt = strings.Replace(*txt, "<*(fc)"+paramSign+"*>", "-1", -1)
+	} else {
+		*txt = strings.Replace(*txt, "<*(c)"+paramSign+"*>", strconv.Itoa(paramValue), -1)
+		*txt = strings.Replace(*txt, "<*(lc)"+paramSign+"*>", strconv.Itoa(paramValue), -1)
+		*txt = strings.Replace(*txt, "<*"+paramSign+"*>", strconv.Itoa(paramValue), -1)
+		*txt = strings.Replace(*txt, "<*(fc)"+paramSign+"*>", strconv.Itoa(paramValue), -1)
 	}
-	//generate maxima script for the current student
-	generate_random_matrix := "A: [fn_0,fn_1,fn_2,fn_3,fn_4]$\nM: matrix(A)$\nfor i: 1 thru 4 do\nM: addrow(M, random_permutation(A))$\ntex1(M);"
-	generate_random_matrix = strings.Replace(generate_random_matrix, "fn_0", string(fn[0]), -1)
-	generate_random_matrix = strings.Replace(generate_random_matrix, "fn_1", string(fn[1]), -1)
-	generate_random_matrix = strings.Replace(generate_random_matrix, "fn_2", string(fn[2]), -1)
-	generate_random_matrix = strings.Replace(generate_random_matrix, "fn_3", string(fn[3]), -1)
-	generate_random_matrix = strings.Replace(generate_random_matrix, "fn_4", string(fn[4]), -1)
 
-	f.Write([]byte(generate_random_matrix))
-	f.Close()
-
-	cmd := exec.Command(`maxima`, `--very-quiet`, `-b`, f.Name())
-	b, err := cmd.Output()
-	if err != nil {
-		panic(err)
-	}
-
-	matrix_latex := re.FindString(string(b))
-	matrix_latex = strings.Replace(matrix_latex, "}", `\end{pmatrix}`, 1)
-	matrix_latex = strings.Replace(matrix_latex, "{", "}", 1)
-	matrix_latex = strings.Replace(matrix_latex, `\`, `\begin{`, 1)
-	txt = strings.Replace(txt, `<*random_matrix*>`, matrix_latex, 1)
-	return txt
-}
-
-//
-func WriteRandomLinearSystem(txt, fn string) string {
-	txt = strings.Replace(txt, `<*random_linear_system*>`, SystemOfEquationsToTex(GenerateHomogenousEquationsFromFN(fn, GenerateVariables("x", len(fn)), 3, 7)), -1)
-	return txt
-}
-
-func WriteVectors(txt, fn string) string {
-	newstr := "\\left.\n\\begin{split}\na&=&"
-	vectors := GenerateVectorsFromFN(fn, 3)
-	newstr = newstr + vectors[0] + "\\\\\nb&=&" + vectors[1] + "\\\\\nc&=&" + vectors[2] + "\n\\end{split}\n\\right."
-	txt = strings.Replace(txt, `<*random_vectors*>`, newstr, 1)
-	return txt
-}
-
-func WriteVector(txt, fn string) string {
-	vector := GenerateVectors(fn, 4, 2, false)
-	intfn, _ := strconv.ParseInt(fn, 10, 0)
-	n := int(intfn) % 3
-	txt = strings.Replace(txt, `<*random_vector*>`, VectorToString(vector[0]), 1)
-	txt = strings.Replace(txt, `<*random_vector*>`, VectorToString(GetPerpendicular4x4(vector[0], n)), 1)
-	txt = strings.Replace(txt, `<*random_vector*>`, VectorToString(vector[1]), 1)
-	return txt
-}
-
-func VectorToString(vector []int) string {
-	svector := "("
-	for i, value := range vector {
-		if i == 0 {
-			svector = svector + strconv.Itoa(value)
-		} else {
-			svector = svector + "," + strconv.Itoa(value)
-		}
-	}
-	svector = svector + ")"
-	return svector
 }
